e02dedde4f0d959523d064226441fdc87544ed30
Lakshit <lakshitlohar02@gmail.com>
2025-06-02 22:41:57 +0530

    added the Value annotation for the api key

--- src/main/java/net/engineeringdigest/journalApp/service/Impl/WeatherServiceImpl.java
+++ src/main/java/net/engineeringdigest/journalApp/service/Impl/WeatherServiceImpl.java
@@ -2,6 +2,7 @@ package net.engineeringdigest.journalApp.service.Impl;
 
 import net.engineeringdigest.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,9 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class WeatherServiceImpl {
 
-    private static final String apiKey = "58edbd770fb8cf486a399bf5d0831c04";
+    @Value("${weather.api.key}")
+    private String apiKey;
+
     private static final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     @Autowired
